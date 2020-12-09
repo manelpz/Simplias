bc91c68e1cf181063e8ae2de77800e403b4c73bf
Emmanuel López Guerrero <manelpz@icloud.com>
2020-12-08 21:30:33 -0600

    adding gmap, kd-tree

--- untitled folder/data/main.js
+++ untitled folder/data/main.js
@@ -1,15 +1,11 @@
-
-const port = 3000
+const port = 3001
 const http = require('http');
-
-
-
-
 
 const server = http.createServer((req, res) => {
     res.write('hello word')
+    console.log('desde el servidor');
     res.end()
-   
+
 })
 
 server.listen(port, '127.0.0.1', ()=>{
